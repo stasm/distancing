86c5a0cf5ed68c1b84def574b4da5e80af9f4d54
Staś Małolepszy <stas@duzodobrze.pl>
2020-03-17 07:44:06 +0100

    Dot blueprint

---
+++ src/blueprints/blu_dot.ts
@@ -0,0 +1,24 @@
+import {float} from "../../common/random.js";
+import {bounce} from "../components/com_bounce.js";
+import {collide} from "../components/com_collide.js";
+import {draw_circle} from "../components/com_draw.js";
+import {health} from "../components/com_health.js";
+import {move} from "../components/com_move.js";
+import {wander} from "../components/com_wander.js";
+import {Blueprint2D} from "../core.js";
+import {Game} from "../game.js";
+
+export function blu_dot(game: Game): Blueprint2D {
+    return {
+        Translation: [float(0, game.CanvasScene.width), float(0, game.CanvasScene.height)],
+        Rotation: float(0, Math.PI * 2),
+        Using: [
+            draw_circle(game.DotRadius),
+            collide(game.DotRadius),
+            move(game.MoveSpeed),
+            bounce(),
+            wander(),
+            health(),
+        ],
+    };
+}
--- src/scenes/sce_stage.ts
+++ src/scenes/sce_stage.ts
@@ -1,11 +1,6 @@
-import {float, set_seed} from "../../common/random.js";
-import {bounce} from "../components/com_bounce.js";
-import {collide} from "../components/com_collide.js";
-import {draw_circle} from "../components/com_draw.js";
-import {health} from "../components/com_health.js";
+import {set_seed} from "../../common/random.js";
+import {blu_dot} from "../blueprints/blu_dot.js";
 import {Has} from "../components/com_index.js";
-import {move} from "../components/com_move.js";
-import {wander} from "../components/com_wander.js";
 import {instantiate} from "../core.js";
 import {Game} from "../game.js";
 import {World} from "../world.js";
@@ -16,32 +11,10 @@ export function scene_stage(game: Game) {
     set_seed(Date.now());
 
     for (let i = 0; i < game.Population - 1; i++) {
-        instantiate(game, {
-            Translation: [float(0, game.CanvasScene.width), float(0, game.CanvasScene.height)],
-            Rotation: float(0, Math.PI * 2),
-            Using: [
-                draw_circle(game.DotRadius),
-                collide(game.DotRadius),
-                move(game.MoveSpeed),
-                bounce(),
-                wander(),
-                health(),
-            ],
-        });
+        instantiate(game, blu_dot(game));
     }
 
-    let patient0 = instantiate(game, {
-        Translation: [float(0, game.CanvasScene.width), float(0, game.CanvasScene.height)],
-        Rotation: float(0, Math.PI * 2),
-        Using: [
-            draw_circle(game.DotRadius),
-            collide(game.DotRadius),
-            move(game.MoveSpeed),
-            bounce(),
-            wander(),
-            health(),
-        ],
-    });
+    let patient0 = instantiate(game, blu_dot(game));
     game.World.Health[patient0].State = "infected";
     game.World.Draw[patient0].Color = game.ColorInfected;
 
